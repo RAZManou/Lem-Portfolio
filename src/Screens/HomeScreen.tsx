7a14f9ace32d72c8d6124e41dfe73d613f33d48d
Razafimisata <ando@hairun-technology.com>
2021-06-09 17:58:04 +0300

    install i18n/redux

---
+++ src/Screens/HomeScreen.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Box from "@material-ui/core/Box";
+
+const HomeScreen = () => {
+  return <Box>This is my Portfolio homepage</Box>;
+};
+
+export default HomeScreen;

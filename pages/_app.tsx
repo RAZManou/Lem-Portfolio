6e9df38b1b529a659a82a73bde5bc160a144fea2
manoa <manoa@wylog.com>
2021-09-13 17:35:12 +0300

    FirstLoadProvider component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,12 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import '../services/i18n';
 import { useTranslation } from 'react-i18next';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { LANGUAGE_CHOICE } from '../constants/localStorages';
 import { Box } from '@material-ui/core';
 import THEMES from '../constants/themes';
 import FONT_SIZES from '../constants/fontSizes';
+import FirstLoadProvider from '../providers/FirstLoadProvider';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const { i18n } = useTranslation();
@@ -19,15 +20,17 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
   return (
     // Default fontSize & fontFamily Here
-    <Box
-      style={{
-        fontSize: FONT_SIZES.default,
-        fontFamily: 'inherit',
-        color: THEMES.dark.white,
-      }}
-    >
-      <Component {...pageProps} />
-    </Box>
+    <FirstLoadProvider>
+      <Box
+        style={{
+          fontSize: FONT_SIZES.default,
+          fontFamily: 'inherit',
+          color: THEMES.dark.white,
+        }}
+      >
+        <Component {...pageProps} />
+      </Box>
+    </FirstLoadProvider>
   );
 };
 export default MyApp;

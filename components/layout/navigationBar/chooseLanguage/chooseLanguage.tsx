1356e79ef4c21098987fd33e1ab9fa3ed2165f6e
manoa <manoa@wylog.com>
2021-09-13 17:36:02 +0300

    disable navigationBar animation after firstLoad

--- components/layout/navigationBar/chooseLanguage/chooseLanguage.tsx
+++ components/layout/navigationBar/chooseLanguage/chooseLanguage.tsx
@@ -1,27 +1,31 @@
-import { Box, makeStyles, createStyles } from "@material-ui/core";
-import React from "react";
-import { FlagEnums } from "../../../../constants/translations";
-import CustomFlag from "./customFlag/customFlag";
-import { useTranslation } from "react-i18next";
+import { Box, makeStyles, createStyles, Slide } from '@material-ui/core';
+import React, { useContext } from 'react';
+import { FlagEnums } from '../../../../constants/translations';
+import CustomFlag from './customFlag/customFlag';
+import { useTranslation } from 'react-i18next';
+import { FirstLoadContext } from '../../../../providers/FirstLoadProvider';
 
 const ChooseLanguage = () => {
   const { i18n } = useTranslation();
+  const { isFirstLoad } = useContext(FirstLoadContext);
   const currentLanguage = i18n.language;
 
   return (
-    <Box
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-      }}
-    >
-      {currentLanguage === FlagEnums.FRENCH ? (
-        <CustomFlag language={FlagEnums.ENGLISH} />
-      ) : (
-        <CustomFlag language={FlagEnums.FRENCH} />
-      )}
-    </Box>
+    <Slide in direction="right" timeout={1000} appear={isFirstLoad}>
+      <Box
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'center',
+        }}
+      >
+        {currentLanguage === FlagEnums.FRENCH ? (
+          <CustomFlag language={FlagEnums.ENGLISH} />
+        ) : (
+          <CustomFlag language={FlagEnums.FRENCH} />
+        )}
+      </Box>
+    </Slide>
   );
 };
 
--- components/layout/navigationBar/navigationBar.tsx
+++ components/layout/navigationBar/navigationBar.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Box } from '@material-ui/core';
 import ChooseLanguage from './chooseLanguage/chooseLanguage';
 import Menu from './menu/menu';
+import { FirstLoadContext } from '../../../providers/FirstLoadProvider';
 
 const NavigationBar = () => {
+  const { setIsFirstLoad } = useContext(FirstLoadContext);
+
+  useEffect(() => {
+    setIsFirstLoad(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Box
       style={{

2a59255593d1ecc3ef9f5820cdebf2a3e9ceef84
manoa <manoa@wylog.com>
2021-09-13 15:07:32 +0300

    styling index with classNames

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { Box, Typography } from "@material-ui/core";
-import Layout from "../components/layout/layout";
-import { useTranslation } from "react-i18next";
-import THEMES from "../constants/themes";
-import { NAME, FIRSTNAME } from "../constants/personalInfos";
+import React, { useState, useEffect } from 'react';
+import {
+  Box,
+  createStyles,
+  makeStyles,
+  Theme,
+  Typography,
+} from '@material-ui/core';
+import Layout from '../components/layout/layout';
+import { useTranslation } from 'react-i18next';
+import THEMES from '../constants/themes';
+import { NAME, FIRSTNAME } from '../constants/personalInfos';
+import RoundButton from '../components/buttons/RoundButton';
 
 const duration = 1000;
-
-const defaultStyle = {
-  transition: `opacity ${duration}ms ease-in-out`,
-  opacity: 0,
-};
 
 const Home = () => {
   const { t } = useTranslation();
@@ -18,6 +20,70 @@ const Home = () => {
   const [opacityTitle, setOpacityTitle] = useState(0);
   const [opacityName, setOpacityName] = useState(0);
   const [opacityDesc, setOpacityDesc] = useState(0);
+
+  const defaultStyle = {
+    transition: `opacity ${duration}ms ease-in-out`,
+    opacity: 0,
+  };
+
+  const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+      container: {
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      jobTitle: {
+        fontSize: 50,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        ...defaultStyle,
+        opacity: opacityTitle,
+      },
+      fullName: {
+        fontSize: 35,
+        textAlign: 'center',
+        marginTop: 35,
+        ...defaultStyle,
+        opacity: opacityName,
+      },
+      introContainer: {
+        textAlign: 'center',
+        marginTop: 70,
+        fontStyle: 'italic',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        ...defaultStyle,
+        opacity: opacityDesc,
+      },
+      intro1And2: {
+        display: 'flex',
+        marginBottom: 10,
+      },
+      firstName: {
+        marginLeft: 5,
+        marginRight: 5,
+        color: THEMES.dark.primary,
+      },
+      flexContainer: {
+        display: 'flex',
+      },
+      intro3: {
+        marginLeft: 5,
+        marginRight: 5,
+        color: THEMES.dark.primary,
+      },
+      buttonsContainer: {
+        display: 'flex',
+        marginTop: 25,
+      },
+    })
+  );
+
+  const classes = useStyles();
 
   useEffect(() => {
     setOpacityTitle(1);
@@ -31,75 +97,28 @@ const Home = () => {
 
   return (
     <Layout>
-      <Box
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          height: "100%",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Typography
-          style={{
-            fontSize: 50,
-            fontWeight: "bold",
-            textAlign: "center",
-            ...defaultStyle,
-            opacity: opacityTitle,
-          }}
-        >
-          {t("home.job.title")}
+      <Box className={classes.container}>
+        <Typography className={classes.jobTitle}>
+          {t('home.job.title')}
         </Typography>
-        <Typography
-          style={{
-            fontSize: 35,
-            textAlign: "center",
-            marginTop: 35,
-            ...defaultStyle,
-            opacity: opacityName,
-          }}
-        >
+        <Typography className={classes.fullName}>
           {`${FIRSTNAME} ${NAME}`}
         </Typography>
-        <Box
-          style={{
-            textAlign: "center",
-            marginTop: 70,
-            fontStyle: "italic",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            ...defaultStyle,
-            opacity: opacityDesc,
-          }}
-        >
-          <Box style={{ display: "flex", marginBottom: 10 }}>
-            {t("home.intro.1")}
-            <Box
-              style={{
-                marginLeft: 5,
-                marginRight: 5,
-                color: THEMES.dark.primary,
-              }}
-            >
-              {FIRSTNAME}
-            </Box>
-            {t("home.intro.2")}
+        <Box className={classes.introContainer}>
+          <Box className={classes.intro1And2}>
+            {t('home.intro.1')}
+            <Box className={classes.firstName}>{FIRSTNAME}</Box>
+            {t('home.intro.2')}
           </Box>
 
-          <Box style={{ display: "flex" }}>
-            <Box
-              style={{
-                marginLeft: 5,
-                marginRight: 5,
-                color: THEMES.dark.primary,
-              }}
-            >
-              {t("home.intro.3")}
-            </Box>
-            {t("home.intro.4")}
+          <Box className={classes.flexContainer}>
+            <Box className={classes.intro3}>{t('home.intro.3')}</Box>
+            {t('home.intro.4')}
           </Box>
+        </Box>
+        <Box className={classes.buttonsContainer}>
+          <RoundButton text="My portfolio" />
+          <RoundButton text="Me" />
         </Box>
       </Box>
     </Layout>

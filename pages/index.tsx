ac5cb2b3d3c1eb8f6308d07f105de04bb5d30646
manoa <manoa@wylog.com>
2021-09-13 17:01:51 +0300

    index animation to materialUI Grow

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { Box, createStyles, makeStyles, Theme } from '@material-ui/core';
+import React from 'react';
+import { Box, createStyles, Grow, makeStyles, Theme } from '@material-ui/core';
 import Layout from '../components/layout/layout';
 import { useTranslation } from 'react-i18next';
 import THEMES from '../constants/themes';
@@ -15,15 +15,6 @@ const Home = () => {
   const { t } = useTranslation();
   const router = useRouter();
 
-  const [opacityTitle, setOpacityTitle] = useState(0);
-  const [opacityName, setOpacityName] = useState(0);
-  const [opacityDesc, setOpacityDesc] = useState(0);
-
-  const defaultStyle = {
-    transition: `opacity ${duration}ms ease-in-out`,
-    opacity: 0,
-  };
-
   const useStyles = makeStyles((theme: Theme) =>
     createStyles({
       container: {
@@ -37,15 +28,11 @@ const Home = () => {
         fontSize: FONT_SIZES.primaryTitle,
         fontWeight: 'bold',
         textAlign: 'center',
-        ...defaultStyle,
-        opacity: opacityTitle,
       },
       fullName: {
         fontSize: FONT_SIZES.secondaryTitle,
         textAlign: 'center',
         marginTop: 35,
-        ...defaultStyle,
-        opacity: opacityName,
       },
       introContainer: {
         textAlign: 'center',
@@ -54,8 +41,6 @@ const Home = () => {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        ...defaultStyle,
-        opacity: opacityDesc,
       },
       intro1And2: {
         display: 'flex',
@@ -83,16 +68,6 @@ const Home = () => {
 
   const classes = useStyles();
 
-  useEffect(() => {
-    setOpacityTitle(1);
-    setTimeout(() => {
-      setOpacityName(1);
-    }, duration);
-    setTimeout(() => {
-      setOpacityDesc(1);
-    }, duration * 2);
-  }, []);
-
   const handleNavigateToAboutMe = () => {
     router.push(ROUTES.ABOUT);
   };
@@ -104,39 +79,47 @@ const Home = () => {
   return (
     <Layout>
       <Box className={classes.container}>
-        <Box className={classes.jobTitle}>{t('home.job.title')}</Box>
-        <Box className={classes.fullName}>
-          {`${FIRSTNAME} ${SECOND_FIRSTNAME} ${NAME}`}
-        </Box>
-        <Box className={classes.introContainer}>
-          <Box className={classes.intro1And2}>
-            {t('home.intro.1')}
-            <Box className={classes.firstName}>{FIRSTNAME}</Box>
-            {t('home.intro.2')}
+        <Grow in={true}>
+          <Box className={classes.jobTitle}>{t('home.job.title')}</Box>
+        </Grow>
+        <Grow in={true} timeout={duration}>
+          <Box className={classes.fullName}>
+            {`${FIRSTNAME} ${SECOND_FIRSTNAME} ${NAME}`}
           </Box>
+        </Grow>
+        <Grow in={true} timeout={duration * 2}>
+          <Box className={classes.introContainer}>
+            <Box className={classes.intro1And2}>
+              {t('home.intro.1')}
+              <Box className={classes.firstName}>{FIRSTNAME}</Box>
+              {t('home.intro.2')}
+            </Box>
 
-          <Box className={classes.flexContainer}>
-            <Box className={classes.intro3}>{t('home.intro.3')}</Box>
-            {t('home.intro.4')}
+            <Box className={classes.flexContainer}>
+              <Box className={classes.intro3}>{t('home.intro.3')}</Box>
+              {t('home.intro.4')}
+            </Box>
           </Box>
-        </Box>
-        <Box className={classes.buttonsContainer}>
-          <RoundButton
-            text="My portfolio"
-            buttonStyle={{
-              borderColor: THEMES.dark.primary,
-            }}
-            onClick={handleNavigateToCareer}
-          />
-          <RoundButton
-            text="Me"
-            buttonStyle={{
-              borderColor: THEMES.dark.primary,
-              backgroundColor: THEMES.dark.primary,
-            }}
-            onClick={handleNavigateToAboutMe}
-          />
-        </Box>
+        </Grow>
+        <Grow in={true} timeout={duration * 3}>
+          <Box className={classes.buttonsContainer}>
+            <RoundButton
+              text="My portfolio"
+              buttonStyle={{
+                borderColor: THEMES.dark.primary,
+              }}
+              onClick={handleNavigateToCareer}
+            />
+            <RoundButton
+              text="Me"
+              buttonStyle={{
+                borderColor: THEMES.dark.primary,
+                backgroundColor: THEMES.dark.primary,
+              }}
+              onClick={handleNavigateToAboutMe}
+            />
+          </Box>
+        </Grow>
       </Box>
     </Layout>
   );

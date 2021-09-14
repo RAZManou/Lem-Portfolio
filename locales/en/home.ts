e9ba7516423da6398a04758d2728ae7efcbc1b9e
manoa <manoa@wylog.com>
2021-09-14 18:07:10 +0300

    change wordings

--- locales/en/home.ts
+++ locales/en/home.ts
@@ -1,6 +1,6 @@
 const home = {
   'home.welcome': 'This is my Portfolio page.',
-  'home.job.title': 'FULLSTACK DEVELOPER',
+  'home.job.title': 'I am a Fullstack Developer',
   'home.intro.1': 'Hello, my name is',
   'home.intro.2': "and I'm a",
   'home.intro.3': 'React, React Native and Node developer',
--- locales/fr/home.ts
+++ locales/fr/home.ts
@@ -1,6 +1,6 @@
 const home = {
   'home.welcome': 'Ceci est ma page Portfolio',
-  'home.job.title': 'DEVELOPPEUR FULLSTACK',
+  'home.job.title': 'Je suis un Développeur Fullstack',
   'home.intro.1': "Bonjour, je m'appelle",
   'home.intro.2': 'et je suis',
   'home.intro.3': 'développeur React, React Native et Node',

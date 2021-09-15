1bd679941cb89fd99a47d70d6b43b7ab31315578
manoa <manoa@wylog.com>
2021-09-15 14:32:38 +0300

    About component with animations

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,10 @@
 import { useTranslation } from 'react-i18next';
 import ScreenTitle from '../components/titles/ScreenTitle';
-import { Box, createStyles, makeStyles, Theme } from '@material-ui/core';
+import { Box, createStyles, makeStyles, Slide, Theme } from '@material-ui/core';
 import React from 'react';
-import profilePic from '../public/me.png';
-import Image from 'next/image';
 import Info from '../components/info/Info';
+import ProfilePic from '../components/images/ProfilePic';
+import { defaultTimeout } from '../constants/animations';
 
 const About = () => {
   const { t } = useTranslation();
@@ -20,9 +20,6 @@ const About = () => {
       profilPic: {
         minWidth: '35%',
       },
-      image: {
-        opacity: 0.8,
-      },
       aboutMeContainer: {
         display: 'flex',
         flexDirection: 'column',
@@ -31,6 +28,11 @@ const About = () => {
       aboutMe: {
         paddingLeft: 100,
       },
+      infoContainer: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'flex-start',
+      },
     })
   );
 
@@ -38,33 +40,37 @@ const About = () => {
 
   return (
     <>
-      <ScreenTitle
-        title={t('menu.about')}
-        subTitle="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text."
-      />
+      <Slide in direction="right" timeout={defaultTimeout / 2}>
+        <Box>
+          <ScreenTitle
+            title={t('menu.about')}
+            subTitle="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text."
+          />
+        </Box>
+      </Slide>
 
       <Box className={classes.container}>
         <Box className={classes.profilPic}>
-          <Image
-            src={profilePic}
-            layout="responsive"
-            alt="Picture of the author"
-            className={classes.image}
-          />
+          <ProfilePic />
         </Box>
         <Box className={classes.aboutMeContainer}>
-          <Box>
-            <ScreenTitle
-              title="Sous titres"
-              subTitle="In id nulla magna. Nullam posuere fermentum mattis. Nunc id dui at sapien faucibus fermentum ut vel diam. Nullam tempus, nunc id efficitur sagittis, urna est ultricies eros, ac porta sem turpis
+          <Slide in direction="left" timeout={defaultTimeout}>
+            <Box>
+              <ScreenTitle
+                title="Sous titres"
+                subTitle="In id nulla magna. Nullam posuere fermentum mattis. Nunc id dui at sapien faucibus fermentum ut vel diam. Nullam tempus, nunc id efficitur sagittis, urna est ultricies eros, ac porta sem turpis
             nunc id efficitur sagittis, urna est ultricies eros, ac porta sem turpis porta sem turpis quis leo. Nulla in feugiat elit"
-              alignText="left"
-              noDivider
-              containerStyle={{ marginTop: 0 }}
-            />
-          </Box>
-          <Box>
+                alignText="left"
+                noDivider
+                containerStyle={{ marginTop: 0 }}
+              />
+            </Box>
+          </Slide>
+          <Box className={classes.infoContainer}>
             <Info label="Name" value="Manoa RAZAFIMISATA" />
+            <Info label="Email" value="manoandoniaina@gmail.com" />
+            <Info label="Téléphone" value="+261 34 49 808 53" />
+            <Info label="LinkedIn" value="Manoa RAZAFIMISATA" />
           </Box>
         </Box>
       </Box>
